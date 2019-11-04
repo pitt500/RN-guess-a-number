50b01b911787924c7e51a022c58034bebb2f592d
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-04 15:53:24 -0700

    Add shadow to container for iOS. Add elevation for Android

--- rn-guess-a-number/screens/StartGameScreen.js
+++ rn-guess-a-number/screens/StartGameScreen.js
@@ -36,7 +36,18 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 8,
+        padding: 20,
+        borderRadius: 10
     }
 });
 
