6e6096e31bf80e61db5606c5f89138583b05eaf3
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-06 11:09:20 -0700

    Styling image with container

--- rn-guess-a-number/screens/GameOverScreen.js
+++ rn-guess-a-number/screens/GameOverScreen.js
@@ -8,20 +8,39 @@ const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <TitleText>The game is over</TitleText>
-            <Image source={require('../assets/success.png')} />
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/success.png')}
+                    style={styles.image}
+                    resizeMode='cover'
+                />
+            </View>
             <BodyText>Number of rounds: {props.rounds}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
-            <Button title="NEW GAME" onPress={props.onRestart}/>
+            <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-  }
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
 });
 
 export default GameOverScreen;
