774684086f9b602dd1ddedcfac2ea049afd28098
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-06 17:21:00 -0700

    Remove unused components

--- rn-guess-a-number/screens/GameScreen.js
+++ rn-guess-a-number/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card';
