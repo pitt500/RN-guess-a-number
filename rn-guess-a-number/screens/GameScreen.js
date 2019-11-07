474e87497aa1d1190b9a4e9e5aa7c22099ef4ff9
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-07 16:14:47 -0700

    Add Dimensions to fix style and magings

--- rn-guess-a-number/screens/GameScreen.js
+++ rn-guess-a-number/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { View, StyleSheet, Alert, ScrollView, FlatList, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer'
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: "row",
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5 ,
         width: 400,
         maxWidth: '90%'
     },
@@ -113,7 +113,8 @@ const styles = StyleSheet.create({
         width: '100%'
     },
     listContainer: {
-        width: '60%',
+        //width: '60%',
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
         flex: 1
     },
     list: {
