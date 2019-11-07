4b1d9b461c65e80c86aadf79fd3e1c5fe0d0503a
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-07 11:56:23 -0700

    Add style to past guesses list and make it scrollable

--- rn-guess-a-number/screens/GameScreen.js
+++ rn-guess-a-number/screens/GameScreen.js
@@ -5,6 +5,7 @@ import { Ionicons } from '@expo/vector-icons';
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card';
 import TitleText from '../components/TitleText';
+import BodyText from '../components/BodyText';
 import MainButton from '../components/MainButton';
 
 const generateRandomNumberBetween = (min, max, exclude) => {
@@ -17,6 +18,14 @@ const generateRandomNumberBetween = (min, max, exclude) => {
         return randomNumber;
     }
 };
+
+const renderListItem = (value, numberOfRound) =>
+    (
+        <View key={value} style={styles.listItem}>
+            <BodyText>#{numberOfRound}</BodyText>
+            <BodyText>{value}</BodyText>
+        </View>
+    );
 
 const GameScreen = props => {
 
@@ -65,13 +74,11 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
-            <ScrollView>
-                {pastGuesses.map(guess => (
-                    <View key={guess}>
-                        <Text>{guess}</Text>
-                    </View>
-                ))}
-            </ScrollView>
+            <View style={styles.list}>
+                <ScrollView>
+                    {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+                </ScrollView>
+            </View>
         </View>
     );
 };
@@ -88,7 +95,20 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 400,
         maxWidth: '90%'
-    }
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    list: {
+        width: '80%',
+        flex: 1
+    },
 });
 
 export default GameScreen;
