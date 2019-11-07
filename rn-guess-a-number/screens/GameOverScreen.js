c97df97166c6b647fd360e1b0daad8e339119384
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-07 16:23:20 -0700

    Fix game over screen for small devices. Use scroll view.

--- rn-guess-a-number/screens/GameOverScreen.js
+++ rn-guess-a-number/screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,25 +8,27 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <TitleText>The game is over</TitleText>
-            <View style={styles.imageContainer}>
-                <Image
-                    source={require('../assets/success.png')} // <-- Only for local images
-                    //source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Summit_of_the_Matterhorn.jpg'}} //<-- Only for web images
-                    style={styles.image}
-                    resizeMode='cover'
-                />
-            </View>
-            <View style={styles.resultContainer}>
-                <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{props.rounds}</Text> rounds to guess the 
+        <ScrollView>
+            <View style={styles.screen}>
+                <TitleText>The game is over</TitleText>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={require('../assets/success.png')} // <-- Only for local images
+                        //source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Summit_of_the_Matterhorn.jpg'}} //<-- Only for web images
+                        style={styles.image}
+                        resizeMode='cover'
+                    />
+                </View>
+                <View style={styles.resultContainer}>
+                    <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{props.rounds}</Text> rounds to guess the
                     number <Text style={styles.highlight}>{props.userNumber}</Text>.
                 </BodyText>
-            </View>
-            <MainButton onPress={props.onRestart} >
-                NEW GAME
+                </View>
+                <MainButton onPress={props.onRestart} >
+                    NEW GAME
             </MainButton>
-        </View>
+            </View>
+        </ScrollView>
     );
 };
 
@@ -37,13 +39,13 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: 30
+        marginVertical: Dimensions.get('window').height / 30
     },
     // Always set width and height for images from web!
     image: {
@@ -56,11 +58,11 @@ const styles = StyleSheet.create({
     },
     resultContainer: {
         marginHorizontal: 30,
-        marginBottom: 20,
+        marginVertical: Dimensions.get('window').height / 60
     },
     resultText: {
         textAlign: 'center',
-        fontSize: 20
+        fontSize: Dimensions.get('window').height < 400 ? 16 : 20
     }
 });
 
