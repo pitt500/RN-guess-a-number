87747365598289fefef90a661a922d32f6fe3f01
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-08 10:43:47 -0700

    Update button width every time user rotates the device.

--- rn-guess-a-number/screens/StartGameScreen.js
+++ rn-guess-a-number/screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     View,
     StyleSheet,
@@ -27,6 +27,7 @@ const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
     const [confirmed, setConfirmed] = useState(false);
     const [selectedNumber, setSelectedNumber] = useState();
+    const [buttonWidth, setButtonWidth] = useState(Dimensions.get('window').width / 4);
 
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
@@ -36,6 +37,18 @@ const StartGameScreen = props => {
         setEnteredValue('');
         setConfirmed(false);
     };
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setButtonWidth(Dimensions.get('window').width / 4);
+        };
+    
+        Dimensions.addEventListener('change', updateLayout);
+
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+        }
+    });
 
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
@@ -85,10 +98,10 @@ const StartGameScreen = props => {
                                 value={enteredValue}
                             />
                             <View style={styles.buttonContainer}>
-                                <View style={styles.button}>
+                                <View style={{width: buttonWidth}}>
                                     <Button title="Reset" onPress={resetInputHandler} color={Colors.accent} />
                                 </View>
-                                <View style={styles.button}>
+                                <View style={{width: buttonWidth}}>
                                     <Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary} />
                                 </View>
                             </View>
@@ -128,6 +141,7 @@ const styles = StyleSheet.create({
         //width: '40%'
         //window is the real space for your app without status bar (or buttons for Android).
         //Dimensions uses the screen size, not the parent view.
+        //This code only runs once in your app cycle.
         width: Dimensions.get('window').width / 4
     },
     input: {
