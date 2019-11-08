8fe8481bb8b3c274213385b57d11b9f2553c4f42
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-08 12:23:57 -0700

    Select style depending of platform

--- rn-guess-a-number/components/Header.js
+++ rn-guess-a-number/components/Header.js
@@ -9,22 +9,36 @@ import Colors from '../constants/colors';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase,
+            ...Platform.select({
+                ios: styles.headeriOS,
+                android: styles.headerAndroid
+            })
+        }}>
             <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Platform.OS === 'android' ?  Colors.primary : 'green',
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? Colors.primary : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+
+    },
+    headeriOS: {
+        borderBottomColor: Colors.primary,
+        borderBottomWidth: 1,
+        backgroundColor: 'green'
+    },
+    headerAndroid: {
+        borderBottomColor: 'transparent',
+        borderBottomWidth: 0,
+        backgroundColor: Colors.primary,
     },
     title: {
         color: 'white'
